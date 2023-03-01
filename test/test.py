1988d7e352fd5b2fd68cc23c6b4d297764bac4b9
amwissal01 <wissalamrani02@gmail.com>
2023-03-01 20:42:24 +0100

    commit9

--- test/test.py
+++ test/test.py
@@ -1,16 +1,15 @@
 import app
-import addition
 #test fonction 1:
 
 def test_addition(self):
         # Vérifie que 1 + 2 = 3
-        self.assertEqual(addition(1, 2), 3)
+        self.assertEqual(app.addition(1, 2), 3)
         
         # Vérifie que -1 + 1 = 0
-        self.assertEqual(addition(-1, 1), 0)
+        self.assertEqual(app.addition(-1, 1), 0)
         
         # Vérifie que 0 + 0 = 0
-        self.assertEqual(addition(0, 0), 0)
+        self.assertEqual(app.addition(0, 0), 0)
 
 
 #test fonction 2
