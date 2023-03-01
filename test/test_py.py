c879526d07b1b7c208198daba9380d10b72e991b
amwissal01 <wissalamrani02@gmail.com>
2023-03-01 23:15:57 +0100

    commit

--- test/test_py.py
+++ test/test_py.py
@@ -6,7 +6,7 @@ import app
 #test fonction 1:
 
 
-def test_addition():
+def test_pyaddition():
 
 
     # Vérifie que 1 + 2 = 3
@@ -23,23 +23,23 @@ def test_addition():
     #test fonction 2
 
 
-def test_soustraction():
+def test_pysoustraction():
 
 
     # Vérifie que 1 - 2 = 3
 
 
-        assert (app.soustraction(1, 2) == 3)
+        assert (app.soustraction(2, 1) == 1)
 
         
     # Vérifie que 5 -1 = 6
 
 
-        assert (app.soustraction(5, 1) == 6)
+        assert (app.soustraction(5, 1) == 4)
 
     
     #teset fonction 3
 
 
-def test_answer2():
+def test_pyanswer2():
        assert( app.deinc(3) == 2)
