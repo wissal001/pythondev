08aff252a581e9973310742336fe02bb704d6ff0
amwissal01 <wissalamrani02@gmail.com>
2023-03-10 02:47:01 +0100

    message

--- test/test_py.py
+++ test/test_py.py
@@ -1,45 +1,15 @@
-
-
 import app
 
 
-#test fonction 1:
-
-
+#test fonction 1
 def test_pyaddition():
-
-
-    # Vérifie que 1 + 2 = 3
-
-
- assert (app.addition(1, 2) == 3)
-
-
-    # Vérifie que -1 + 1 = 0
-
-
- assert (app.addition(5, 1) == 6)
-        
+    assert(app.addition(1, 2) == 3)
+    assert(app.addition(5, 1) == 6)
 #test fonction 2
-
-
 def test_pysoustraction():
-
-
-    # Vérifie que 1 - 2 = 3
-
-
- assert (app.soustraction(2, 1) == 1)
-
-        
-    # Vérifie que 5 -1 = 6
-
-
-assert (app.soustraction(6, 1) == 5)
-
+    assert(app.soustraction(2, 1) == 1)
+    assert(app.soustraction(6, 1) == 5)
     
-    #teset fonction 3
-
-
+#tset fonction 3
 def test_pyanswer2():
- assert( app.deinc(3) == 2)
+    assert app.deinc(3) == 2
